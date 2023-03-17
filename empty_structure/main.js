2107722bb51c5869192410d366faedc5f1f7e606
R0ck3n <r0ck3n@hotmail.com>
2023-03-17 12:43:18 +0100

    progress fonction

--- empty_structure/main.js
+++ empty_structure/main.js
@@ -1,8 +1,24 @@
-const num1 = prompt("rentrer 1er num")
- if (num1 < 0 || num1 >10) {
-     console.error('erreur')
- }else {
-     for (let i = num1; i >= 0; i--) {
-         console.log(i)
-     }
- }
+let number = Math.floor(Math.random() * 10) + 1;
+
+console.log(number)
+
+const isRight = () =>{
+    window.alert('gg wp')
+}
+const guess = ()=>{
+for (let i = 0; i < 3; i++) {
+    const essai = Number(prompt('Devinez le chiffre de 1 à 10'))
+
+    if (i === 2) window.alert('you lose')
+    else if (essai < number) window.alert('need upper number')
+    else if (essai > number) window.alert('need lower number')
+    else if (essai === number) {
+        isRight()
+        break
+    }
+
+}
+}
+
+
+guess()
