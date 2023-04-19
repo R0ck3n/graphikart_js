42e1a885ea29e0d95ce0739d926086c223a22617
Rocken <r0ck3n@hotmail.com>
2023-04-19 17:08:47 +0200

    test nvx PC

--- empty_structure/main.js
+++ empty_structure/main.js
@@ -1,1 +1,17 @@
+const fetchage = async () => {
+  const r = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_limit=10",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ title: "Mon premier article" }),
+    }
+  );
 
+  return r.ok ? r.json() : new Error("Fail");
+};
+
+fetchage().then((users) => console.log(users));
