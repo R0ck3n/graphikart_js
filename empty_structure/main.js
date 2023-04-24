a5065689cc6a845a826929b42162408fcc7bd787
Rocken <r0ck3n@hotmail.com>
2023-04-24 22:01:08 +0200

    Exo todo

--- empty_structure/main.js
+++ empty_structure/main.js
@@ -1,34 +1,31 @@
-const app = document.querySelector("#app");
-app.innerHTML = "Chargement...";
-
-const articles = async () => {
-  const r = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=5", {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-    },
+(function destroy() {
+  const trash = document.querySelectorAll(".bi-trash");
+  trash.forEach((el) => {
+    el.addEventListener("click", () => {
+      el.parentNode.parentNode.remove();
+    });
   });
+})();
 
-  if (!r.ok) {
-    app.innerHTML = "Impossible de Charger la page !";
-    return;
+const fetchTodo = async () => {
+  try {
+    const r = await fetch(
+      "https://jsonplaceholder.typicode.com/todos?_limit=5",
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "applicatio/json",
+        },
+      }
+    );
+    if (!r.ok) {
+      throw new Error("Erreur serveur");
+    }
+    return r.json();
+  } catch (e) {
+    console.error(e);
   }
-
-  app.innerHTML = "";
-  return r.json();
 };
 
-articles().then((data) =>
-  data.forEach((element) => {
-    const newArticle = document.createElement("article");
-    const newH2 = document.createElement("h2");
-    const newp = document.createElement("p");
-
-    newH2.append(element.title);
-    newp.append(element.body);
-
-    newArticle.append(newH2, newp);
-    app.append(newArticle);
-    console.log(element);
-  })
-);
+fetchTodo().then((e) => console.log(e));
