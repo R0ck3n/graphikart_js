a33e397d7b773761cf5848b444027acc106ee7cb
Rocken <r0ck3n@hotmail.com>
2023-05-09 14:55:55 +0200

    event

--- empty_structure/main.js
+++ empty_structure/main.js
@@ -1,1 +1,13 @@
-console.log("test");
+/**
+ *
+ * @param {PointerEvent} e
+ */
+const reveal = (e) => {
+  document.querySelectorAll(".spoiler").forEach((spoiler) => {
+    spoiler.classList.remove("spoiler");
+  });
+};
+
+document.querySelectorAll(".spoiler").forEach((el) => {
+  el.addEventListener("click", reveal);
+});
